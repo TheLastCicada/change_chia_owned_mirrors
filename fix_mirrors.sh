503aa185897e54062a54adaa84d3016356481462
Zachary Brown <zachary.g.brown@gmail.com>
2024-02-15 13:09:54 -0800

    Added more output

--- fix_mirrors.sh
+++ fix_mirrors.sh
@@ -13,7 +13,7 @@ do
     coin_id=$(nice -n 19 chia data get_mirrors --id ${sub} | jq -r --arg badURL "${badURL}" '.mirrors[] | select(.ours == true and any(.urls[]; contains($badURL))) | .coin_id')
     if [ ! -z "$coin_id" ]
     then
-    echo "Deleting and recreating mirror for coin_id: ${coin_id}"
+        echo "Deleting and recreating mirror for coin_id: ${coin_id}"
         confirmedCount=$(nice -n 19 chia wallet get_transactions --no-paginate | grep "Status: Confirmed" | wc -l)
         totalCount=$(nice -n 19 chia wallet get_transactions --no-paginate | grep "Status:" | wc -l)
         while [ $confirmedCount != $totalCount ]
@@ -21,8 +21,9 @@ do
             echo "Waiting for transaction to be confirmed..."
             confirmedCount=$(nice -n 19 chia wallet get_transactions --no-paginate | grep "Status: Confirmed" | wc -l)
             totalCount=$(nice -n 19 chia wallet get_transactions --no-paginate | grep "Status:" | wc -l)
-            sleep 8
+            sleep 5
         done
+        echo "Deleting mirror for ${coin_id}"
         nice -n 19 chia data delete_mirror -c ${coin_id}
         confirmedCount=0
         totalCount=1
@@ -31,8 +32,9 @@ do
             echo "Waiting for transaction to be confirmed..."
             confirmedCount=$(nice -n 19 chia wallet get_transactions --no-paginate | grep "Status: Confirmed" | wc -l)
             totalCount=$(nice -n 19 chia wallet get_transactions --no-paginate | grep "Status:" | wc -l)
-            sleep 8
+            sleep 5
         done
+        echo "Adding mirror for store-id ${sub}"
         nice -n 19 chia data add_mirror --id ${sub} --amount ${amount} --url ${newUrl}
         confirmedCount=0
         totalCount=1
@@ -41,7 +43,7 @@ do
             echo "Waiting for transaction to be confirmed..."
             confirmedCount=$(nice -n 19 chia wallet get_transactions --no-paginate | grep "Status: Confirmed" | wc -l)
             totalCount=$(nice -n 19 chia wallet get_transactions --no-paginate | grep "Status:" | wc -l)
-            sleep 8
+            sleep 5
         done
     else
         echo "No bad mirrors here"
