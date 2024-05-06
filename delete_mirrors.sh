09ffa42dd18cbf14df0c7fdad92339e6fe594766
Zachary Brown <zachary.g.brown@gmail.com>
2024-05-06 14:40:39 -0700

    Add configurable fee for mirror deletion

--- delete_mirrors.sh
+++ delete_mirrors.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Fee to include with each transaction (in XCH)
+fee=0
+
 subscriptions_array=( $(nice -n 19 chia data get_subscriptions | jq -r .store_ids[]) )
 
 for sub in "${subscriptions_array[@]}"
@@ -10,7 +13,7 @@ do
     if [ ! -z "$coin_id" ]
     then
         echo "Deleting and mirror for coin_id: ${coin_id}"
-        nice -n 19 chia data delete_mirror -c ${coin_id}
+        nice -n 19 chia data delete_mirror -c ${coin_id} -m ${fee}
     else
         echo "None of our mirrors here"
     fi
