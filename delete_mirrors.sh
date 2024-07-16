777e4a8d5eb36d6cef8d0aa0c7e5fc6592c10c45
Zachary Brown <zachary.g.brown@gmail.com>
2024-07-16 14:33:50 -0700

    handle case where multiple mirrors are owned

--- delete_mirrors.sh
+++ delete_mirrors.sh
@@ -10,10 +10,17 @@ do
     echo "Subscription ID: ${sub}"
     coin_id=''
     coin_id=$(nice -n 19 chia data get_mirrors --id ${sub} | jq -r '.mirrors[] | select(.ours == true) | .coin_id')
-    if [ ! -z "$coin_id" ]
+    if [ ! -z "$coin" ]
     then
-        echo "Deleting and mirror for coin_id: ${coin_id}"
-        nice -n 19 chia data delete_mirror -c ${coin_id} -m ${fee}
+            coin_ids=($coin_id) # Handle a situation with multiple owned mirrors....
+            echo "coin_id is ${coin_id} and array coin_ids is ${coin_ids[*]}"
+            for coin in "${coin_ids[@]}"
+            do
+                echo "Deleting and mirror for coin_id: ${coin}"
+                set -x
+                nice -n 19 chia data delete_mirror -c ${coin} -m ${fee}
+                set +x
+            done
     else
         echo "None of our mirrors here"
     fi
