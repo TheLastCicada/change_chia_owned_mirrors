4cc468558139410551dc0f1457421ca0b04408d6
Zachary Brown <zachary.g.brown@gmail.com>
2024-07-17 12:05:02 -0700

    Improve delete_mirrors.sh logic and output

--- delete_mirrors.sh
+++ delete_mirrors.sh
@@ -10,16 +10,13 @@ do
     echo "Subscription ID: ${sub}"
     coin_id=''
     coin_id=$(nice -n 19 chia data get_mirrors --id ${sub} | jq -r '.mirrors[] | select(.ours == true) | .coin_id')
-    if [ ! -z "$coin" ]
+    if [ ! -z "$coin_id" ]
     then
             coin_ids=($coin_id) # Handle a situation with multiple owned mirrors....
-            echo "coin_id is ${coin_id} and array coin_ids is ${coin_ids[*]}"
             for coin in "${coin_ids[@]}"
             do
                 echo "Deleting and mirror for coin_id: ${coin}"
-                set -x
                 nice -n 19 chia data delete_mirror -c ${coin} -m ${fee}
-                set +x
             done
     else
         echo "None of our mirrors here"
